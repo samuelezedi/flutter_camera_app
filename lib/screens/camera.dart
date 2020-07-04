7e2c9fa12b424bc7db054a9a55cdb8d440b44f42
Samuel Ezedi <samuelezedi@gmail.com>
2020-07-04 13:01:14 +0100

    camera screen upgrades

---
+++ lib/screens/camera.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {\
+
+  CameraController cameraController;
+  List cameras;
+  int selectedCameraIndex;
+  String imgPath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
