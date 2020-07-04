19133bf5c59a87106200dd276e18d9301e3d085e
Samuel Ezedi <samuelezedi@gmail.com>
2020-07-04 14:23:49 +0100

    upgraded preview screeen

--- lib/screens/preview.dart
+++ lib/screens/preview.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PreviewScreen extends StatefulWidget {
@@ -17,8 +18,34 @@ class _PreviewScreenState extends State<PreviewScreen> {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: true,
-
       ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Image.file(File(widget.imgPath),fit: BoxFit.cover,),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: double.infinity,
+                height: 60,
+                color: Colors.black,
+                child: Center(
+                  child: IconButton(
+                    icon: Icon(Icons.share,color: Colors.white,),
+                    onPressed: (){
+
+                    },
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 
